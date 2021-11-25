c11f5b5b43c6173c93106f9b32b7383b66b62bfb
Microsoft Greve <microsoft@software-greve.de>
2021-11-25 19:45:07 +0100

    ...

---
+++ Constants.cs
@@ -0,0 +1,45 @@
+﻿namespace Testaufgabe_Dynamics_Consulting_PlugIns
+{
+  public static class Constants
+  {
+    public const string NO_NUMBER = "[^0-9]";
+
+    public static class Organization
+    {
+      public const string URI = @"https://crm90aio.dcscloud.de/testtask/XRMServices/2011/Organization.svc";
+
+      public const string USERNAME = "testuser";
+
+      public const string PASSWORD = "j9OAV85qYh7Karq";
+
+      public const string DOMAIN = "crm90aio";
+    }
+
+    public static class Entity
+    {
+      public const string ACCOUNT = "account";
+
+      public const string CONTACT = "contact";
+
+      public const string TARGET = "Target";
+    }
+
+    public static class Account
+    {
+      public const string NAME = "name";
+    }
+
+    public static class Contact
+    {
+      public const string FIRSTNAME = "firstname";
+
+      public const string LASTNAME = "lastname";
+
+      public const string PARENT_CUSTOMER_ID = "parentcustomerid";
+
+      public const string EMAILADRESS1 = "emailaddress1";
+
+      public const string TELEPHONE2 = "telephone2";
+    }
+  }
+}
---
+++ PreAccountCreatedPlugIn.cs
@@ -0,0 +1,54 @@
+﻿namespace Testaufgabe_Dynamics_Consulting_PlugIns
+{
+  public class PreAccountCreatedPlugIn : Microsoft.Xrm.Sdk.IPlugin
+  {
+    /// <summary>
+    /// Throws InvalidPluginExecutionException if there may be a Name-Doublette
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    public void Execute(System.IServiceProvider serviceProvider)
+    {
+      Microsoft.Xrm.Sdk.IPluginExecutionContext pluginExecutionContext = (Microsoft.Xrm.Sdk.IPluginExecutionContext)
+        serviceProvider.GetService(typeof(Microsoft.Xrm.Sdk.IPluginExecutionContext));
+
+      if (!pluginExecutionContext.InputParameters.Contains(Constants.Entity.TARGET) ||
+          !(pluginExecutionContext.InputParameters[Constants.Entity.TARGET] is Microsoft.Xrm.Sdk.Entity))
+      {
+        return;
+      }
+
+      Microsoft.Xrm.Sdk.Entity entity = (Microsoft.Xrm.Sdk.Entity)pluginExecutionContext.InputParameters[Constants.Entity.TARGET];
+
+      if (entity.LogicalName != Constants.Entity.ACCOUNT)
+      {
+        return;
+      }
+
+      if (!entity.Attributes.Contains(Constants.Account.NAME))
+      {
+        throw new Microsoft.Xrm.Sdk.InvalidPluginExecutionException("The Name of the Account is necessary!");
+      }
+
+      Microsoft.Xrm.Sdk.IOrganizationServiceFactory organizationServiceFactory = (Microsoft.Xrm.Sdk.IOrganizationServiceFactory)
+        serviceProvider.GetService(typeof(Microsoft.Xrm.Sdk.IOrganizationServiceFactory));
+
+      Microsoft.Xrm.Sdk.IOrganizationService organizationService =
+        organizationServiceFactory.CreateOrganizationService(pluginExecutionContext.UserId);
+
+      Microsoft.Xrm.Sdk.Query.QueryExpression queryExpression = new Microsoft.Xrm.Sdk.Query.QueryExpression(Constants.Entity.ACCOUNT);
+
+      queryExpression.Criteria.AddCondition(
+        Constants.Account.NAME,
+        Microsoft.Xrm.Sdk.Query.ConditionOperator.Equal, entity.GetAttributeValue<string>(Constants.Account.NAME));
+
+      Microsoft.Xrm.Sdk.EntityCollection entityCollection = organizationService.RetrieveMultiple(queryExpression);
+
+      if (entityCollection == null || entityCollection.Entities == null || entityCollection.Entities.Count < 1)
+      {
+        return;
+      }
+
+      throw new Microsoft.Xrm.Sdk.InvalidPluginExecutionException($"Account with name: {entity.GetAttributeValue<string>(Constants.Account.NAME)} still exists!");
+    }
+  }
+}
